2064033d59599c8941382d7605302965f330bae0
Rashmi Pai <raspai@publicisgroupe.net>
2023-07-05 16:44:49 -0400

    Added alert functionality

--- albertson/src/Login.js
+++ albertson/src/Login.js
@@ -1,6 +1,29 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Login = () => {
+  const [user, setUser] = useState('')
+  const [pass, setPass] = useState('')
+  const [invalid, setInvalid] = useState(false)
+
+  function validateLogin(){
+    if(user.trim().length === 0 && pass.trim().length === 0){
+      //alert("Invalid Username or Password")
+      setInvalid(true)
+
+      setTimeout(() => {
+        setInvalid(false)
+      }, 4000)
+    }
+  }
+
+  const handleUserChange = event => {
+    setUser(event.target.value)
+  }
+  const handlePassChange = event => {
+    setUser(event.target.value)
+  }
+
     return (
       <div className="split-container">
         <div className="left-side">
@@ -9,9 +32,14 @@ const Login = () => {
         <div className="right-side">
           <h1>Login </h1>
           <h4>Sign in with your company email</h4>
-          <input className='email' placeholder='Email'></input>
-          <input className='pass' placeholder='Password'></input>
-          <button className='loginBtn'>Login</button> 
+          <input onChange={handleUserChange} className='email' placeholder='Email'></input>
+          <input onChange={handlePassChange} className='pass' placeholder='Password'></input>
+          <button className='forgetPass'>Forget Password?</button>
+          {invalid && 
+            <div>
+              <h5>Incorrect username or password</h5>
+            </div>}
+          <button onClick={validateLogin} className='loginBtn'>Login</button> 
         </div>
       </div>
     )
