5117515012702534e3008deccd25668e8b66f3d2
Sarah Ukani <ukanisarah@gmail.com>
2023-07-07 11:16:26 -0400

    login page closer to figma design

--- albertson/src/Login.js
+++ albertson/src/Login.js
@@ -1,22 +1,26 @@
 import React from 'react';
+import firstpage from './firstpage.png';
+import logo from './logo.svg';
 
 const Login = () => {
     return (
       <div className="split-container">
         <div className="left-side">
-          <h2>Sarah</h2>
         </div>
         <div className="right-side">
-          <h1>Login </h1>
-          <h4>Sign in with your company email</h4>
-          <input className='email' placeholder='Email'></input>
+        <img src={logo} alt="Logo" />
+      
+          <div>
+          <input className='Enter your email' placeholder='Email'></input>
+          </div>
+          <div>
           <input className='pass' placeholder='Password'></input>
+          </div>
           <button className='loginBtn'>Login</button> 
         </div>
       </div>
     )
   }
-  
   export default Login;
 
 
