8ae3d7a82743945685efc6392fb804c652d87312
Rashmi Pai <raspai@publicisgroupe.net>
2023-07-10 09:40:21 -0400

    added uploading ability to main

--- albertson/src/App.js
+++ albertson/src/App.js
@@ -7,10 +7,9 @@ import logo from './logo.svg';
 function App() {
   return (
     <div>
-      {/* <h1>Hi, welcome back!</h1>
+      <h1>Hi, welcome back!</h1>
       <h4>Sign in with your company email</h4>
-      <Login></Login> */}
-      <Main></Main>
+      <Login></Login>
       <i class="fa fa-envelope-open"></i>
       
     <div className="App">
