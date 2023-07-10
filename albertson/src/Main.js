27c7f1c4bb25f5fb504bbc4ca5b754628f250aff
Rashmi Pai <raspai@publicisgroupe.net>
2023-07-10 14:02:25 -0400

    foundation of main change according to figma

--- albertson/src/Main.js
+++ albertson/src/Main.js
@@ -8,7 +8,13 @@ export default function Main(){
             <img className="icon" src="https://th.bing.com/th/id/R.ea0d38e91f172b985c948f989e98c39f?rik=NR0pbQNo1c8XMw&riu=http%3a%2f%2fcdn.onlinewebfonts.com%2fsvg%2fimg_357118.png&ehk=cUAdriibLw7xEDkfIXfwIR3GAAijwWJWZIpc7ctVbmo%3d&risl=&pid=ImgRaw&r=0"/>
             
             <div className='AI_GenBox'>
-                <input className="chatBoxInput"></input>            
+                <p className="firstText">Start with a <b><i>detailed description</i></b></p>
+                <p className="secondText">Here are some <b>recommendations:</b></p>
+                <textarea className="detailedDescrip" cols="50"></textarea>
+                <button className="generateBtn">Generate</button>
+                <p className="thirdText">Here are some <b>relevant ads</b> pulled from the website:</p>
+                <div className="reccomendations"></div>
+
             </div>
             <div className="Upload">
                 <Uploader></Uploader>
