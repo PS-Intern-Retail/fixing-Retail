2e308adccbac05a258cc14c070b6f5cbf1fd81e6
Andrew Mao <andrewmao@Andrews-MacBook-Pro-8.local>
2023-07-12 11:37:09 -0400

    added database class for frontend queries

--- albertson/src/data/database.js
+++ albertson/src/data/database.js
@@ -4,8 +4,6 @@ export default class Database {
 
     static async postUserSession(loginRequest) {
         console.log(loginRequest);
-        console.log("I CALLED THIS FUNCTION");
-
         try {
             const response = await fetch(`${backendOrigin}/users/login`, {
                 method: "POST",
@@ -14,16 +12,12 @@ export default class Database {
                 },
                 body: JSON.stringify(loginRequest),
             });
-
             if (response.status === 401) {
-                console.log("I AM HERE IN THE 401");
                 throw new Error("Invalid login credentials");
             }
-
             if (response.ok) {
                 return response.json();
             } else {
-                console.log("THE RESPONSE IS NOT OKAY");
                 throw new Error("An error occurred");
             }
         } catch (error) {
