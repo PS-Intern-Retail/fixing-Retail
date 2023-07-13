72b9960a07e1e6de87cb597dfd3d85555ec88f21
Idris Lawal <idrlawal@publicisgroupe.net>
2023-07-13 10:04:38 -0400

    playlist

--- albertson/src/App.js
+++ albertson/src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 import Main from './mainComp/Main.js';
 import Login from './loginComp/Login.js';
 import './loginComp/Login.css';
-import ImagePlaylistPage from './ImagePlaylistPage';
-
 //import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 //import StoreLocation from './StoresLocation';
@@ -18,8 +16,6 @@ function App() {
         <h1>Hi, welcome back!</h1>
         <h4>Sign in with your company email</h4>
         <Login />
-        <ImagePlaylistPage />
-
         <i className="fa fa-envelope-open"></i>
       </div>
   );
--- albertson/src/ImagePlaylistPage.js
+++ albertson/src/ImagePlaylistPage.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import './ImagePlaylistPage.css'; 
+import React, { useState } from 'react';
+import './ImagePlaylistPage.css';
 
 const ImagePlaylistPage = () => {
-  // Sample data of customer ad recommendation URLs
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handlePanelClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  const handleViewAllPlaylists = () => {
+    // Implement your logic for "View All Playlists" button here
+    console.log('View All Playlists button clicked');
+  };
+
   const recommendationURLs = [
     'https://th.bing.com/th/id/OIP.6tSSldzehqBowRd3mDzsDgAAAA?pid=ImgDet&rs=1',
     'https://th.bing.com/th/id/OIP.6tSSldzehqBowRd3mDzsDgAAAA?pid=ImgDet&rs=1',
@@ -12,16 +22,50 @@ const ImagePlaylistPage = () => {
   ];
 
   return (
-    <div className="image-playlist-container">
-      <div className="image-playlist">
-        {recommendationURLs.map((url, index) => (
-          <div className="image-slide" key={index}>
-            <img src={url} alt={`Image ${index + 1}`} />
-          </div>
-        ))}
-      </div>
-      <div className="centered-button">
-        <button>View All Playlists</button>
+    <div className="container">
+      <div
+        className={`panel ${activeIndex === 0 ? 'active' : ''}`}
+        style={{
+          backgroundImage: `url(${recommendationURLs[0]})`,
+        }}
+        onClick={() => handlePanelClick(0)}
+      ></div>
+
+      <div
+        className={`panel ${activeIndex === 1 ? 'active' : ''}`}
+        style={{
+          backgroundImage: `url(${recommendationURLs[1]})`,
+        }}
+        onClick={() => handlePanelClick(1)}
+      ></div>
+
+      <div
+        className={`panel ${activeIndex === 2 ? 'active' : ''}`}
+        style={{
+          backgroundImage: `url(${recommendationURLs[2]})`,
+        }}
+        onClick={() => handlePanelClick(2)}
+      ></div>
+
+      <div
+        className={`panel ${activeIndex === 3 ? 'active' : ''}`}
+        style={{
+          backgroundImage: `url(${recommendationURLs[3]})`,
+        }}
+        onClick={() => handlePanelClick(3)}
+      ></div>
+
+      <div
+        className={`panel ${activeIndex === 4 ? 'active' : ''}`}
+        style={{
+          backgroundImage: `url(${recommendationURLs[4]})`,
+        }}
+        onClick={() => handlePanelClick(4)}
+      ></div>
+        <div>
+      <button className="view-all-button" onClick={handleViewAllPlaylists}>
+        View All Playlists
+      </button>
       </div>
     </div>
   );
