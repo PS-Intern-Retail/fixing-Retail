8fe587ec066c5697e8be8e8fcabfa237c22f6199
Rashmi Pai <raspai@publicisgroupe.net>
2023-07-13 16:58:56 -0400

    added store selection path

--- albertson/src/App.js
+++ albertson/src/App.js
@@ -4,6 +4,7 @@ import Main from './mainComp/Main.js';
 import Login from './loginComp/Login.js';
 import './loginComp/Login.css';
 import logo from './images/logo.svg';
+import StoresLocation from './StoresLocation'
 import {Routes, Route} from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={ <Login /> }/>
         <Route path="/main" element={ <Main /> }/>
+        <Route path="/sl" element={ <StoresLocation /> }/>
       </Routes>
     </div>
   );
