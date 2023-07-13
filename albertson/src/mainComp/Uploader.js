92ef40d108cdd6edbeadfce7aec46bc6a369462c
sukani3 <sukani3@gatech.edu>
2023-07-13 15:15:22 -0400

    routing and uploading changes

--- albertson/src/mainComp/Uploader.js
+++ albertson/src/mainComp/Uploader.js
@@ -1,26 +1,104 @@
-import React from 'react'
-import './Uploader.css'
-import { useState } from 'react'
-import {MdCloudUpload, MdDelete} from 'react-icons/md'
-import { AiFillFileImage } from 'react-icons/ai'
+import React, { useState, useRef } from 'react';
+import './Uploader.css';
+import { MdCloudUpload, MdDelete } from 'react-icons/md';
+import { AiFillFileImage } from 'react-icons/ai';
 
-export default function Uploader(){
-    const [image, setImage] = useState(null)
-    return (
-        <main>
-            <form action=""
-            onClick={() => document.querySelector(".picture-input").click()}>
-                <input type="file" accept="image/*" className="picture-input" hidden/>
+export default function Uploader() {
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const fileInputRef = useRef(null);
 
-                {image ?
-                    <img src={image} width={60} height={60}/>
-                :
-                <>
-                    <MdCloudUpload color='#FFFFFF' border="#1475CF" size={60} />
-                    <p className='uploadText'>Select or Drag an Image to Upload</p>
-                </>
-                }
-            </form>
-        </main>
-    )
+  const handleImageUpload = async (event) => {
+    const files = event.target.files;
+    setLoading(true);
+    try {
+      const newImages = await Promise.all(
+        Array.from(files).map(async (file) => ({
+          name: file.name,
+          url: await uploadImage(file),
+        }))
+      );
+      setImages((prevImages) => [...prevImages, ...newImages]);
+    } catch (error) {
+      console.error('Error uploading image:', error);
+    } finally {
+      setLoading(false);
+      event.target.value = null; // Reset the file input value
+    }
+  };
+
+  const uploadImage = (file) => {
+    return new Promise((resolve, reject) => {
+      // Simulating image upload delay with setTimeout
+      setTimeout(() => {
+        const imageUrl = URL.createObjectURL(file);
+        resolve(imageUrl);
+      }, 2000);
+    });
+  };
+
+  const handleDeleteImage = (index) => {
+    const newImages = [...images];
+    newImages.splice(index, 1);
+    setImages(newImages);
+  };
+
+  const handleAddMoreImages = () => {
+    fileInputRef.current.click();
+  };
+
+  return (
+    <main>
+      <div className={`upload-widget ${loading ? 'empty' : images.length === 0 ? 'empty' : 'non-empty'}`}>
+        {images.length === 0 && !loading ? (
+          <>
+            <MdCloudUpload color="#FFFFFF" border="#1475CF" size={60} />
+            <label htmlFor="file-input" >
+              Select Images to Upload
+            </label>
+          </>
+        ) : null}
+        <input
+          type="file"
+          accept="image/*"
+          className="picture-input"
+          ref={fileInputRef}
+          onChange={handleImageUpload}
+          multiple
+          style={{ display: 'none' }}
+          id="file-input"
+          disabled={loading}
+        />
+    
+        {images.length > 0 && (
+          <button className="add-more-button" onClick={handleAddMoreImages} disabled={loading}>
+            Add More Images
+          </button>
+        )}
+      </div>
+
+      {loading && <div class="loading-container">
+    <div class="loading-bar"></div>
+    <div class="loading-text">Uploading...</div>
+  </div>
+}
+
+     
+      {!loading && (
+        <div className="image-list-container">
+          <div className="image-list">
+            {images.map((image, index) => (
+              <div className="image-item" key={index}>
+                <img src={image.url} alt={image.name} width={20} height={20} />
+                <div className="image-item-overlay">
+                  <span>{image.name}</span>
+                  <MdDelete onClick={() => handleDeleteImage(index)} />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </main>
+  );
 }
