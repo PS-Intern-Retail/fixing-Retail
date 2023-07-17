bfcb0a053d4a03175d20b9fd95ce58afd8d5814d
Idris Lawal <idrlawal@publicisgroupe.net>
2023-07-17 14:12:39 -0400

    Committing local changes

--- albertson/src/App.js
+++ albertson/src/App.js
@@ -7,16 +7,12 @@ import logo from './images/logo.svg';
 import StoresLocation from './storeLocationComp/StoresLocation'
 import Verification from './storeLocationComp/verification';
 import {Routes, Route} from "react-router-dom";
+import ViewPlaylist from './ViewPlaylist';
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={ <Login /> }/>
-        <Route path="/main" element={ <Main /> }/>
-        <Route path="/sl" element={ <StoresLocation /> }/>
-        <Route path="/verification" element={ <Verification /> }/>
-      </Routes>
+    <ViewPlaylist/>
     </div>
   );
 }
