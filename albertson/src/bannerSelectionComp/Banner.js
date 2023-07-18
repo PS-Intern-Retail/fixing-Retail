c2ca1d2dd68a1057fb8b81c2115e223f383524a3
Rashmi Pai <raspai@publicisgroupe.net>
2023-07-18 16:32:13 -0400

    changed routing system

--- albertson/src/bannerSelectionComp/Banner.js
+++ albertson/src/bannerSelectionComp/Banner.js
@@ -1,21 +1,28 @@
 import './Banner.css'
 import Database from '../data/database';
 import { useNavigate } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import { useState } from 'react'
 
 export default function Banner(){
     const navigate = useNavigate()
+    const { state } = useLocation()
+    // const curUser = state.user
+
     const [choice, setChoice]= useState(
         new Array(3).fill(false)
     )
     const [index, setIndex] = useState(0)
     const storeList = ["Acme", "Safeway", "Pavilions"]
-
+    
+    
     function handleBack(){
         navigate('/')
     }
     function handleNext(){
-        navigate('/sl', {state: {storeName: storeList[index]}})
+        navigate('/sl'
+        , {state: {storeName: storeList[index]
+        }})
     }
     function choiceClick(index){
         let newChoice = new Array(3).fill(false)
@@ -35,6 +42,7 @@ export default function Banner(){
         <div className="left-side">
         </div>
         <div className="right-side">
+            {(!state) ? <h1 className="welcome">Welcome!</h1> : <h1 className="welcome">Welcome {state.user.firstName}!</h1>}
             <h1 className="header">Banner Selection</h1>
             <p className="subHeader">Select ONE banner from the list below.</p>
             <ul className="storeList">
