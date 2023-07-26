2de99b05e411ff6292b2b1a3182a1ebdaf859c98
Rashmi Pai <raspai@publicisgroupe.net>
2023-07-26 16:02:32 -0400

    more responsiveness

--- albertson/src/mainComp/Main.js
+++ albertson/src/mainComp/Main.js
@@ -7,8 +7,6 @@ import { useNavigate } from 'react-router-dom'
 import Icon from './Icon.js';
 import { Link, useLocation } from 'react-router-dom';
 
-
-
 export default function Main(){
     const { state } = useLocation()
 
@@ -21,9 +19,8 @@ export default function Main(){
         storeList = state.storeList
     }
 
-    console.log(storeList)
     return(
-        <div>
+        <div className="mainDiv">
             <Icon storeName={storeName} user={user}/>
             <div className='AI_GenBox'>
                 <AiComp storeList={storeList}></AiComp>
@@ -31,17 +28,6 @@ export default function Main(){
             <div className="Upload">
                 <Uploader></Uploader> 
             </div>
-
-            {/* <div className='viewPlaylist'>  
-                <button className="viewPlaylistBtn">View All Playlists</button>
-            </div>
-
-            <div className='viewPlaylist'>
-                <button className="viewPlaylistBtn"
-                onClick={navigateToPlaylist}
-                >View All Playlists
-                </button>
-            </div> */}
 
         <div className='viewPlaylist'>
             <Playlist storeName={storeName}></Playlist>
